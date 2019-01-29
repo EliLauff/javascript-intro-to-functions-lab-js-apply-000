3f44eeaafaff5f4011843496c000c12e523b3c4e
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-01-29 22:27:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string === string.toLowerCase){
+  if(string === string.toLowerCase()){
       var response1 = "I can't hear you!"
       return response1
   }
-  else if(string === string.toUpperCase){
+  else if(string === string.toUpperCase()){
       var response2 = "YES INDEED!"
       return response2
   }
